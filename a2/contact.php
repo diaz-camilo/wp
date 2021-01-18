659cc018dc0fbb0782250a68db7026eda6094cb9
Camilo <cajadi86@hotmail.com>
2021-01-18 21:15:54 +1100

    redesign layout and styles contact form and index

--- a2/contact.php
+++ a2/contact.php
@@ -11,7 +11,7 @@
     <meta itemprop="description" content="This site records the letters home during WW1 from ANZAC Douglas Raymond Baker, from September 1914 after he joined up in Gympie, Queensland, Australia to May 1918." />
     <meta id="meta-tag-description" property="og:description" content="This site records the letters home during WW1 from ANZAC Douglas Raymond Baker, from September 1914 after he joined up in Gympie, Queensland, Australia to May 1918." />
 
-    <meta http-equiv="refresh" content="3" >
+    <!-- <meta http-equiv="refresh" content="3" > -->
 
     <title>ANZAC Douglas Raymond Baker</title>
     
@@ -25,31 +25,55 @@
 
     <header>
       <img src='../../media/avatar.jpg' alt="D.R. Baker F.Co one of the soldiers photographed in The Queenslander Pictorial supplement to The Queenslander 1914">
-      <p>ANZAC Douglas Raymond Baker ~ Letters Home</p>      
+      <p>ANZAC Douglas Raymond Baker - Letters Home</p>      
     </header>
     <nav>
       
-      <a href='index.php'>Home</a>
+    <a href='index.php'>Home</a>
       <a href='intro.php'>Introduction</a>
       <a href='places.php'>Places</a>
       <a href='contact.php'>contact</a>
       <a href='letters.php'>Letters & Post Cards</a>
       <a href='descriptions.php'>Descriptions of Battle Action</a>      
       <a href='links.php'>Links to related Materials</a>
+      
     </nav>
 
     
 
     <main>
-      <article>
-        <!-- <div class="greetings"></div> -->
-      <h2>Hello and welcome,</h2>
-      <p>This year is the centenary of the birth of the ANZAC legend. As such, many people, particularly young people, are looking for ways to connect with people of that period and inparticular, those who created the ANZAC legend.</p>
-      <p>This site presents the letters of Douglas Raymond Baker, who came from Gympie, Queensland, Australia. He enlisted in August 1914 and during the years that followed, he wrote letters and post cards to his family at home. In them, he describes much of the goings-on of the life of an ANZAC, his feeling and opinions, and experiences while visiting his relatives in England during leave.</p>
-      <p>They start from the beginning of basic training in Brisbane in August 1914 and end in May 1918.</p>
-      <p>They are offered here so that others may get an understanding of life as an ANZAC and get a sense of what life on the battlefield was like.</p>
-      <p>From the menu on the left, read the Introduction to set the scene. Then, to start reading the letters, click on Letter and Post Cards in the menu on the left. All the letters are searchable using the  search bar at the top right of this page.</p>
-      </article>
+      <form class="form" action="https://titan.csit.rmit.edu.au/~e54061/wp/testcontact.php" method="post" target="_blank">
+        
+                  <article>
+                  <div class="grid-container">
+          <div class="grid-label"><label for="name">Name: </label></div>
+          <div class="grid-input"><input type="text" id="name" name="name" placeholder="Jon Doe" required value=""/></div>
+          <div class="grid-label"><label for="email">Email: </label></div>  
+          <div class="grid-input"><input type="email" id="email" name="email" placeholder="your-email@email.com" required value=""/></div>
+          <div class="grid-label"><label for="mobile">Mobile: </label></div>
+          <div class="grid-input"><input type="tel" id="mobile" name="mobile" minlength="10" maxlength="10" placeholder="0404123456" required value="" /></div>  
+          <div class="grid-label"><label for="subject">Subject:</label></div>
+          <div class="grid-input"><input type="text" id="subject" name="subject" placeholder="Subject" value=""/></div>
+          <div class="grid-label"><label  for="message">Message: </label></div> 
+          <div class="grid-input"><textarea id="message" rows="10" name="message" placeholder="Type your message here" value=""></textarea></div>
+          <div class="grid-item"></div>
+          <div class="grid-input">
+            
+          <label class="container">Remember me
+          <input type="checkbox" name="remember-me" id="remember-me" checked="checked" />
+          <span class="checkmark"></span>
+          </label>
+            
+          
+          <!-- <input type="checkbox" value="remember-me"/>Remember me</div>  -->
+          <div class="grid-item"></div>
+          <div class="grid-input"><input type="submit" name="send" value="Send"></div> 
+        </div>
+        </article>
+                  
+                   
+                    
+                  </form>
     </main>
 
     <footer>
@@ -62,3 +86,7 @@
 
   </body>
 </php>
+
+
+
+
