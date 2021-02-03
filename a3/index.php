6a728fb0259f82eb00c3c60809b6b6eb257b2447
Camilo <cajadi86@hotmail.com>
2021-02-03 21:39:34 +1100

    A3 first commit - copied file into a3 floder

---
+++ a3/index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE php>
+<php lang='en'>
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="title" content="ANZAC Douglas Raymond Baker" />
+    <meta itemprop="name" content="ANZAC Douglas Raymond Baker" />
+    <meta property="og:title" content="ANZAC Douglas Raymond Baker" />
+    <meta name="description" content="This site records the letters home during WW1 from ANZAC Douglas Raymond Baker, from September 1914 after he joined up in Gympie, Queensland, Australia to May 1918." />
+    <meta itemprop="description" content="This site records the letters home during WW1 from ANZAC Douglas Raymond Baker, from September 1914 after he joined up in Gympie, Queensland, Australia to May 1918." />
+    <meta id="meta-tag-description" property="og:description" content="This site records the letters home during WW1 from ANZAC Douglas Raymond Baker, from September 1914 after he joined up in Gympie, Queensland, Australia to May 1918." />
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=BioRhyme&family=Libre+Baskerville&family=Libre+Franklin&family=Roboto&display=swap" rel="stylesheet">
+
+    <title>ANZAC Douglas Raymond Baker</title>
+
+    <!-- Keep wireframe.css for debugging, add your css to style.css -->
+    <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
+    <link id='stylecss' type="text/css" rel="stylesheet" href="style.css?t=<?= filemtime("style.css"); ?>">
+    <script src='../wireframe.js'></script>
+  </head>
+
+  <body>
+    <header>
+      <div class="tooltip"><img src='../../media/avatar.jpg' alt="D.R. Baker F.Co one of the soldiers photographed in The Queenslander Pictorial supplement to The Queenslander 1914"><span class="tooltip_text">
+          <p>(Photograph courtesy of : John Oxley Library, State Library of Queensland [Image number: 702692-19141024-s0023-0027])</p>
+        </span></div>
+      <p>ANZAC <span>Douglas Raymond</span> Baker<br><span>Letters Home</span></p>
+    </header>
+    <span class="poppy"></span>
+    <label for="show_hide_nav"><span id="menu"></span></label>
+    <input type="checkbox" name="show_hide_nav" id="show_hide_nav">
+
+    <nav>
+      <a href='index.php'>Home</a>
+      <a href='places.php'>Places</a>
+      <a href='contact.php'>contact</a>
+      <a href='letters.php'>Letters & Post Cards</a>
+      <a href='descriptions.php'>Descriptions of Battle Action</a>
+      <a href='links.php'>Links to related Materials</a>
+    </nav>
+
+    <main>
+      <article>
+        <h2>Hello and welcome,</h2>
+        <section>
+          <p>This year is the centenary of the birth of the ANZAC legend. As such, many people, particularly young people, are looking for ways to connect with people of that period and inparticular, those who created the ANZAC legend.</p>
+          <p>This site presents the letters of Douglas Raymond Baker, who came from Gympie, Queensland, Australia. He enlisted in August 1914 and during the years that followed, he wrote letters and post cards to his family at home. In them, he describes much of the goings-on of the life of an ANZAC, his feeling and opinions, and experiences while visiting his relatives in England during leave.</p>
+          
+          <p class="description"><img src='../../media/S.S._Omrah.jpg' alt="Orient Steam Navigation Company liner Omrah in Brisbane in 1914. She became a troop ship in the First World War and was torpedoed and sunk off Cape Spartivento in 1918."> Orient Steam Navigation Company liner Omrah in Brisbane in 1914. She became a troop ship in the First World War and was torpedoed and sunk off Cape Spartivento in 1918. <br>By <a rel="nofollow" href="https://hdl.handle.net/10462/deriv/124738">Item is held by John Oxley Library, State Library of Queensland.</a>, Public Domain, <a href="https://commons.wikimedia.org/w/index.php?curid=12375890">Link</a></p>
+          
+          <p>They start from the beginning of basic training in Brisbane in August 1914 and end in May 1918.</p>
+          <p>They are offered here so that others may get an understanding of life as an ANZAC and get a sense of what life on the battlefield was like.</p>
+          <p>From the menu on the left, read the Introduction to set the scene. Then, to start reading the letters, click on Letter and Post Cards in the menu on the left. All the letters are searchable using the search bar at the top right of this page.</p>
+        </section>
+      </article>
+      <article>
+        <h2>Introduction</h2>
+        <section>
+          <h3>By Douglas Richard Baker / (son of Douglas Raymond Baker)</h3>
+          <p>These are copies of letters written by my father Douglas Raymond Baker during the First World War (1914-1918) to his family in Gympie. I have three thick exercise books in which the letters were copied in hand writing by my Aunt Alice, his sister. I understand that this was done so his letters could be sent on to other members of the family. I don’t know if the originals are still in existence, probably not.</p>
+          <p>In the early letters Alice has not included the name of the person they were written to but later she writes at the beginning, “Letter to Dot”, “Letter to Mother”, etc. and later still copies the original, “Dear Al”, “Dear Mag”, etc.. Likewise the endings are often not laid out fully as we would do but Al may have done this or Dad may have been saving space on the page.</p>
+          <p>Some letters are slightly out of date order in the books but as most of these are around the time his father died I have put them in the correct order as this is crucial to understanding the sense of the contents. A lot will be missing – many, many ships were sunk but thanks to Aunt Alice we have these copies of the ones that did get through.</p>
+          <p class="description"><img src='../../media/original_enlistment_paper.jpeg' alt="Original record of enlistment of Raymond Douglas Baker"> <a href="https://recordsearch.naa.gov.au/SearchNRetrieve/Interface/ViewImage.aspx?B=3009496&S=1">Original service record - Baker Douglas Raymond : SERN 692 : POB Gympie QLD : POE Gympie QLD : NOK F Baker Edward</a></p>
+          <p>Where Dad used brackets in a letter I have used [ ] style and where I have made any comment or explanation I have used ( ) and Italics.</p>
+          <p>Where he uses the word “gay” it is used in the true sense, bright, happy, carefree, etc., This is the original meaning of the word before it became associated with the homosexual community.</p>
+          <p>The amounts of money are, of course, in Pounds, shillings and pence but as these are out of date and we have no pounds sign in the computer I have written them in, sometimes showing the decimal equivalents. One Pound equalled $2, one shilling equalled 10 cents and one penny equalled a little under one cent. After a while I stopped putting the equivalents in as they had cno significance unless you knew the relative cost of things then and now. A shilling – equivalent to ten cents might actually alent to ten cents might actually buy buy ten or twenty dollars worth of goods now.</p>
+          <p>Some abbreviations and terms:</p>
+          <ul>
+            <li>Coy = Company(part of a Battalion)</li>
+            <li>Batt. = Battalion.</li>
+            <li>Col. = Colonel.</li>
+            <li>Sergt. = Sergeant.</li>
+            <li>Capt. = Captain</li>
+            <li>Lieut. = Lieutenant (pronounced “Lef-tenant”)</li>
+            <li>Q.M. = Quarter Master.</li>
+            <li>Comp. = also Company.</li>
+            <li>PC = Post Card.(not Personal Computer!)</li>
+            <li>CB = Confined to Barracks (leave stopped as a punishment)</li>
+            <li>big hits = top ranked officers.</li>
+            <li> brass hats = top ranked officers.</li>
+            <li>dry canteen = bar and/or cafeteria, etc. where no alcohol is served.</li>
+            <li>wet canteen = bar and/or cafeteria, etc. where alcohol is available.</li>
+            <li>sapping = digging a tunnel or deep trench to approach or undermine an enemy position.</li>
+            <li>L. Horse = Light Horse.</li>
+            <li>good nick or great nic= good health</li>
+            <li>bonsorful = wonderful, marvellous. (in 2009speak, “awesome”, “cool”.)</li>
+            <li>M. G. S. = Machine Gun Section.</li>
+            <li>Territorials = soldiers of the English Volunteer Reserve.</li>
+            <li>Terriers = Territorials</li>
+            <li>Bomb = unless dropped from an aeroplane these would have been Mills Bombs – an early version of the hand grenade.</li>
+            <li>Blighty = England.</li>
+            <li>Duke = Dad’s/family’s dog.</li>
+            <li>O.R. = Orderly Room.</li>
+          </ul>
+        </section>
+        <section>
+          <h3>Additional Comments by Ian Stuart Baker / (son of Douglas Richard Baker)</h3>
+          <p>As the grandson of the Douglas Raymond, I'd like to share some thoughts with modern readers. Apart from the language issues that my Dad highlights, it would also help to recognise that in Grandfathers day, the letter was the only means of international communications for average people (telegrams were expensive and used rarely and international telephone calls, rarer still). That's why these letters are so important and why their content gives us window into their lives.</p>
+          <p class="description"><img src='../../media/ANZAC_day_poster.jpg' alt="ANZAC day poster commemoration, drawing of a soldier with gun and packpack over oval drawing of gallipoli beach with troops landing. date at the bottom april 25 1916"> <a href="https://bytesdaily.blogspot.com/2010/04/first-anzac-day-1916.html">ANZAC day poster from 1916</a></p>
+          <p>From discussions with my Dad, it has came to light that the content of letters to his direct family deliberately leave out much of the dreadful suffering and drudgery experienced by the diggers. This intentional self-editing was intended to allay the fears and concerns of those at home, inparticular, his mother and sisters.</p>
+          <p>Finally, let me express a debt of thanks to Great Aunt Alice for diligently transcribing the original letters for without this effort, we would not have the material we do today. Also, my gratitude to my father for his work transforming the hand-written script into a typed paper record and then into electronic format, making my part in this infinitely easier.</p>
+        </section>
+      </article>
+    </main>
+
+    <footer>
+      <p>&copy; <script>
+          document.write(new Date().getFullYear());
+        </script> Camilo Jaramillo Diaz, s3820251. <a href="https://github.com/diaz-camilo"><img src='../../media/GitHub-Mark-Light-32px.png' alt="github logo, links to Camilo's github repo"></a> ibak6837@bigpond.net.au<br>Last modified <?= date("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>. </p>
+      <p>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia. </p>
+      <p><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></p>
+    </footer>
+
+  </body>
+</php>
